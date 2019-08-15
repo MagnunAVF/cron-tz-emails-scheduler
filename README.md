7e2df4e0d50c868810d81ef42e93993342c97211
Magnun A V F <magnunavf@gmail.com>
2019-08-15 14:17:40 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Cron Tz Emails Scheduler
+Simple REST API to schedule email send based on receiver CRON expression and Timezone.
+Created with: Spring, Maven, MySQL, Quartz, Docker.
+Based on:
+* https://www.callicoder.com/spring-boot-quartz-scheduler-email-scheduling-example/
+* https://github.com/abhishek70/spring-boot-docker-rest-api
---
+++ cron-tz-emails-scheduler/src/main/java/com/example/crontzemailsscheduler/CronTzEmailsSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.crontzemailsscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronTzEmailsSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronTzEmailsSchedulerApplication.class, args);
+	}
+
+}
